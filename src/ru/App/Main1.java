025c96204e8f357f8afd664c4c6e84f127c85f0e
mikoffsky <mikoffsky@gmail.com>
2020-09-07 16:09:46 +0300

    Задание 2

--- src/ru/App/Main1.java
+++ src/ru/App/Main1.java
@@ -1,7 +1,16 @@
 package ru.App;
 
+import java.util.Scanner;
+
 public class Main1 {
     public static void main(String[] args) {
-        System.out.println("Hello x5");
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value= scanner.nextInt();
+        int a = value/100;
+        int b = ((value - (a * 100)) / 10);
+        int c = value - (a * 100) - (b * 10);
+        int d = (a + b + c);
+        System.out.println(d);
     }
 }
