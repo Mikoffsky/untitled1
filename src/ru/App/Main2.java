a2f40d9900cbb622476a479a5cfb77ede1db463f
mikoffsky <mikoffsky@gmail.com>
2020-09-07 16:28:38 +0300

    Задание 3

--- src/ru/App/Main2.java
+++ src/ru/App/Main2.java
@@ -1,7 +1,19 @@
 package ru.App;
 
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        if ( value <0){
+            System.out.println(value);
+        }
+        else
+        {
+            int i = value + 1;
+            System.out.println(i);
+        }
     }
 }
